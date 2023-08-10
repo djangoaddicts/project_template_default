68c5e51cf0beb3de2977f06d61014720a704b39c
David Slusser <dbslusser@gmail.com>
2023-08-09 22:02:37 -0700

    fixing urls init

--- core/views.py
+++ core/views.py
@@ -14,11 +14,11 @@ class RegisterUser(generic.CreateView):
 
 class Index(HandyHelperIndexView):
     """render the project index page"""
-    title = """Welcome to <span class="text-primary">{{ PROJECT_NAME }}</span><span class="text-secondary"></span>!"""
+    title = """Welcome to <span class="text-primary">{{ project_name }}</span><span class="text-secondary"></span>!"""
     subtitle = "Select an option below to get started"
     item_list = [
         {
-            "url": "/rest/",
+            "url": "/rest",
             "icon": "fas fa-download",
             "title": "APIs",
             "description": "List available RESTful APIs",
